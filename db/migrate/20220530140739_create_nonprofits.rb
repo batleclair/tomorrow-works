b747903714b4004ad2383dc2836b1d5ec795b201
Jean Eudes Nallatamby <njeaneudes@gmail.com>
2022-05-31 20:06:34 +0200

    big pbs with tables

---
+++ db/migrate/20220530140739_create_nonprofits.rb
@@ -0,0 +1,16 @@
+class CreateNonprofits < ActiveRecord::Migration[7.0]
+  def change
+    create_table :nonprofits do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.text :mission
+      t.string :legal_form
+      t.string :address
+      t.string :sector
+      t.text :description
+      t.string :siret
+
+      t.timestamps
+    end
+  end
+end
