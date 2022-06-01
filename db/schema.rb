7e74fd01a8776ffe9a1cec638f271975ee2f7da5
Baptiste Clair <baptisteclair@gmail.com>
2022-06-01 17:34:37 +0200

    fixing candidate sign-in path and adding nonprofit mission to nonprofits in seed

--- db/schema.rb
+++ db/schema.rb
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_01_144155) do
     t.string "siret"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
     t.index ["user_id"], name: "index_nonprofits_on_user_id"
   end
 
