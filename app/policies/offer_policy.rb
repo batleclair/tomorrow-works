1455f2e4aceb1b1b15ac2b66c9f58be0d2163d35
Baptiste Clair <baptisteclair@gmail.com>
2022-06-01 17:00:32 +0200

    customizing sign-up and sign-in routes depending on user status

---
+++ app/policies/offer_policy.rb
@@ -0,0 +1,36 @@
+class OfferPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.where(nonprofit) == user.nonprofit
+    end
+  end
+end
