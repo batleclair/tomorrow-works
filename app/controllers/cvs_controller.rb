19e3a572abc72f8c47c5c264e96b787ba570de42
Mouna Tricha <mouna.tricha@live.fr>
2022-06-06 18:13:17 +0200

    migrate

--- app/controllers/cvs_controller.rb
+++ app/controllers/cvs_controller.rb
@@ -1,32 +1,36 @@
 class CvsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :index, :show]
+
+  def new
+    @cv = Cv.new
+    @candidate = Candidate.find_by(user_id: current_user.id)
+    authorize @cv
+  end
+
   def show
     @cv = CV.find(params[:id])
     authorize @cv
   end
 
-  def new
-    @cv = CV.new
-    authorize @cv
-  end
-
   def create
-    @cv = CV.new(cv_params)
-    @cv.candidate = Candidate.find_by(user_id: current_user.id)
+    @cv = Cv.new(cv_params)
+    authorize @cv
+    @candidate = Candidate.find_by(user_id: current_user.id)
+    @cv.candidate_id = @candidate.id
     if @cv.save
-      redirect_to candidate_path
+      redirect_to candidate_path(@candidate)
     else
       render :new, unprocessable: :entity
     end
-    authorize @cv
   end
 
-  def my_cvs
-    @candidate = Candidate.find(params[:candidate_id])
-    @cvs = @candidate.cvs
-    authorize @cvs
-  end
+  # def my_cvs
+  #   @candidate = Candidate.find(params[:candidate_id])
+  #   @cvs = @candidate.cvs
+  #   authorize @cvs
+  # end
 
   def cv_params
-    params.require(:cv).permit(:titre, :entreprise, :date_debut, :date_fin, :description)
+    params.require(:cv).permit(:titre, :entreprise, :date_début, :date_fin, :description)
   end
 end
