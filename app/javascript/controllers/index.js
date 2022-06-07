aa7f65609f8ba496a2e67274be366d6ac880ca0b
Mouna Tricha <mouna.tricha@live.fr>
2022-06-07 18:10:11 +0200

    map for non profit show

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import AnimationController from "./animation_controller.js"
+application.register("animation", AnimationController)
+
 import CardsController from "./cards_controller.js"
 application.register("cards", CardsController)
 
@@ -13,11 +16,11 @@ application.register("find-company", FindCompanyController)
 import HelloController from "./hello_controller.js"
 application.register("hello", HelloController)
 
+import MapController from "./map_controller.js"
+application.register("map", MapController)
+
 import SearchAssoController from "./search_asso_controller.js"
 application.register("search-asso", SearchAssoController)
 
 import SortableController from "./sortable_controller.js"
 application.register("sortable", SortableController)
-
-import AnimationController from "./animation_controller.js"
-application.register("animation", AnimationController)
