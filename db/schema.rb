00075989fb4ab17bbf7e7be393e8a595bd05c56b
Mouna Tricha <mouna.tricha@live.fr>
2022-06-09 11:37:34 +0200

    salary

--- db/schema.rb
+++ db/schema.rb
@@ -106,13 +106,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_08_204950) do
     t.index ["candidate_id"], name: "index_cvs_on_candidate_id"
   end
 
-  create_table "experiences", force: :cascade do |t|
-    t.bigint "candidate_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["candidate_id"], name: "index_experiences_on_candidate_id"
-  end
-
   create_table "nonprofits", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name"
@@ -174,7 +167,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_08_204950) do
   add_foreign_key "candidatures", "users"
   add_foreign_key "companies", "users"
   add_foreign_key "cvs", "candidates"
-  add_foreign_key "experiences", "candidates"
   add_foreign_key "nonprofits", "users"
   add_foreign_key "offers", "nonprofits"
   add_foreign_key "offers", "users"
